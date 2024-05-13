81cec36698da141a55da8e996bfd52ced2bcdb0f
ogisetiawan <ogisetiawan21@gmail.com>
2024-05-13 22:20:12 +0700

    ✅ add notes module 1

--- module/Modules - Noted.php
+++ module/Modules - Noted.php
@@ -5,3 +5,24 @@
  * @Last Modified by:   ogi.setiawan 
  * @Last Modified time: 2024-05-07 17:28:49 
  */
+
+
+//@ Module 1
+//~ Memahami Dasar dasar pryoek
+//? proses perencanaan, pengorganisasian, dan pengendalian sumber daya untuk mencapai tujuan proyek tertentu dalam batasan waktu, anggaran, dan kualitas yang telah ditentukan.
+//? projek manager akan banyak digunakan, di beberapa sektor industri; construction manager proyek, it proyek manager
+//~ Tahapan Manajemen Proyek
+/// Inisiasi: Mendefinisikan tujuan proyek, ruang lingkup, dan pemangku kepentingan.
+/// Perencanaan: Mengembangkan rencana proyek, termasuk jadwal, anggaran, dan sumber daya.
+/// Eksekusi: Melaksanakan rencana proyek dan memantau kemajuan.
+/// Pengendalian: Mengidentifikasi dan mengatasi masalah, serta memastikan proyek tetap sesuai rencana.
+/// Penutupan: Menyelesaikan proyek dan mengevaluasi hasilnya
+//~ Mempelajari karir dalam bidang manajemen proyek
+//? Manajer program: Mengelola sekelompok proyek yang terkait atau mirip satu sama lain dan menangani koordinasi proyek-proyek tersebut. Mereka memfasilitasi komunikasi yang efektif di antara masing-masing manajer proyek dan memberikan dukungan jika diperlukan. Mereka juga membantu menciptakan dan mengelola tujuan jangka panjang untuk organisasi mereka.
+//? Manajer Portoflio: mengoordinasikan berbagai program untuk memastikan program-program tersebut berjalan sesuai rencana dan memenuhi inisiatif strategis organisasi tersebut. Manajer portofolio mengawasi semua proyek dan program dalam organisasi tersebut dan memprioritaskan pekerjaan yang diperlukan. 
+/// Peran Agile
+///? Scrum Master: Mengoordinasikan dan memandu tim Scrum. Memiliki pengetahuan tentang kerangka kerja Agile dan Scrum serta mampu mengajarkan orang lain tentang nilai-nilai dan prinsip-prinsip Scrum
+//? Pemilik Produk: Menetapkan arah pengembangan dan kemajuan produk
+/// Ingatlah bahwa keterampilan yang Anda pelajari dalam satu industri bisa diterapkan pada industri lain. Misalnya, Anda mungkin memiliki pengalaman sebagai teknisi perangkat lunak, tetapi tertarik untuk mengejar karier di bidang manajemen proyek. Anda dapat menerapkan apa yang telah Anda pelajari selama bekerja di bidang teknis
+//~ Jargon yang umum dalam manajemen proyek
+//? Analitis, Asertif/Tegas, Perhatian terhadap detail, Manajemen risiko, Pengembangan solusi

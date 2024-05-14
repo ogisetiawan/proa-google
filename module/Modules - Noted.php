b1ef410fd43613798739beda007950b11f599314
ogisetiawan <ogisetiawan21@gmail.com>
2024-05-14 16:12:20 +0700

    ✅ add notes module 2

--- module/Modules - Noted.php
+++ module/Modules - Noted.php
@@ -12,11 +12,11 @@
 //? proses perencanaan, pengorganisasian, dan pengendalian sumber daya untuk mencapai tujuan proyek tertentu dalam batasan waktu, anggaran, dan kualitas yang telah ditentukan.
 //? projek manager akan banyak digunakan, di beberapa sektor industri; construction manager proyek, it proyek manager
 //~ Tahapan Manajemen Proyek
-/// Inisiasi: Mendefinisikan tujuan proyek, ruang lingkup, dan pemangku kepentingan.
-/// Perencanaan: Mengembangkan rencana proyek, termasuk jadwal, anggaran, dan sumber daya.
-/// Eksekusi: Melaksanakan rencana proyek dan memantau kemajuan.
-/// Pengendalian: Mengidentifikasi dan mengatasi masalah, serta memastikan proyek tetap sesuai rencana.
-/// Penutupan: Menyelesaikan proyek dan mengevaluasi hasilnya
+//? Inisiasi: Mendefinisikan tujuan proyek, ruang lingkup, dan pemangku kepentingan.
+//? Perencanaan: Mengembangkan rencana proyek, termasuk jadwal, anggaran, dan sumber daya.
+//? Eksekusi: Melaksanakan rencana proyek dan memantau kemajuan.
+//? Pengendalian: Mengidentifikasi dan mengatasi masalah, serta memastikan proyek tetap sesuai rencana.
+//? Penutupan: Menyelesaikan proyek dan mengevaluasi hasilnya
 //~ Mempelajari karir dalam bidang manajemen proyek
 //? Manajer program: Mengelola sekelompok proyek yang terkait atau mirip satu sama lain dan menangani koordinasi proyek-proyek tersebut. Mereka memfasilitasi komunikasi yang efektif di antara masing-masing manajer proyek dan memberikan dukungan jika diperlukan. Mereka juga membantu menciptakan dan mengelola tujuan jangka panjang untuk organisasi mereka.
 //? Manajer Portoflio: mengoordinasikan berbagai program untuk memastikan program-program tersebut berjalan sesuai rencana dan memenuhi inisiatif strategis organisasi tersebut. Manajer portofolio mengawasi semua proyek dan program dalam organisasi tersebut dan memprioritaskan pekerjaan yang diperlukan. 
@@ -26,3 +26,61 @@
 /// Ingatlah bahwa keterampilan yang Anda pelajari dalam satu industri bisa diterapkan pada industri lain. Misalnya, Anda mungkin memiliki pengalaman sebagai teknisi perangkat lunak, tetapi tertarik untuk mengejar karier di bidang manajemen proyek. Anda dapat menerapkan apa yang telah Anda pelajari selama bekerja di bidang teknis
 //~ Jargon yang umum dalam manajemen proyek
 //? Analitis, Asertif/Tegas, Perhatian terhadap detail, Manajemen risiko, Pengembangan solusi
+
+
+//@ Module 2
+//~ Mengekplore bagaimana manajer proyek memberikan nilai tambahan
+/// Nilai menajer proyek
+//? prioritas: Manajer proyek membantu anggota tim mengidentifikasi dan memecah tugas-tugas besar menjadi langkah-langkah yang lebih kecil
+//? Delegasi: Manajer proyek mencocokkan tugas dengan individu yang paling mampu menyelesaikan pekerjaan.
+//? Komunikasi yang efektif: Manajer proyek menjaga komunikasi yang terbuka dan teratur dengan tim dan pemangku kepentingan mereka
+/// Bagaimana manajer proyek berdampak pada organisasi
+//? Berfokus pada pelanggan, pelanggan internal (skateholders, menejemen) dan external (customer)
+//? Membangun tim yang hebat; Manajer proyek yang sukses mengetahui hal itu dan meluangkan waktu untuk memahami motivasi, kekuatan, dan kelemahan setiap orang
+//? Membina hubungan dan komunikasi; 
+//? Mengelola proyek;
+//? Mendobrak hambatan; 
+/// Belajar lebih banyak tentang peran dan tanggung jawab manajer proyek
+//? Planning and orgnize, managing task and budgeting/controlling (issues hambatan and cost)
+/// Tanggung jawab yang memanfaatkan keterampilan interpersonal
+//? Keterampilan interpersonal; perilaku yang dgunakan untuk berinteraksi dengan orang lain, seperti komunikasi, mendengarkan secara aktif, dan kepemimpinan
+//? Seperti Mengajar dan membimbing; menjadi mentor dan support ke pada team
+//? Membangun hubungan; 
+//? Mengendalikan perubahan; mendokumentasikan ekspektasi awal proyek dan mengidentifikasi perubahan yang diminta dengan jelas, dan melindungi team agar proyek tidak berubah2
+//? Memberdayakan tim Anda; Memberi tim Anda kemampuan untuk bekerja secara langsung dengan para pemangku kepentingan, seperti ide, agar mereka bersemangat
+//~ Belajar lebih banyak tentang peran dan tanggung jawab manajer proyek
+/// Peran manajer proyek dalam sebuah tim
+//? Manajer proyek bukanlah manajer langsung dari anggota tim proyek, tetapi bertanggung jawab untuk membimbing mereka
+//? Manajer proyek harus mampu meminta pertanggungjawaban semua anggota tim atas tugas yang diberikan, memastikan bahwa masalah dan risiko dilacak dan terlihat
+/// Bekerja dengan tim lintas fungsi
+//? Tim lintas fungsi anggota tim yang memiliki latar belakang, jenis keahlian, dan fungsi pekerjaan yang berbeda (T-shaped)
+//? Mampu berkomunikasi dengan jelas dengan anggota tim, memperjelas tujuan proyek, mendapatkan anggota tim dengan keterampilan yang tepat, mengukur kemajuan tim, dan mengenali upaya anggota tim
+/// Elita: Keseharian sebagai seorang manajer proyek
+//? Seorang manajer proyek yang hebat harus memiliki kombinasi unik dari berbagai keterampilan.
+//? Tetap terorganisir adalah kunci untuk menjadi manajer proyek yang efektif.
+//? Komunikasi yang efektif sangat penting untuk keberhasilan proyek.
+//? Fleksibilitas adalah kunci untuk menghadapi tantangan yang tidak terduga
+//~ Memperoleh keterampilan inti dari manajer proyek yang sukses
+/// Keterampilan inti seorang manajer proyek
+//? Membuat keputusan: Manajer proyek harus dapat membuat keputusan yang tepat dan tepat waktu untuk menjaga proyek tetap pada jalurnya. Ini termasuk mengumpulkan informasi dari anggota tim dan menggunakan wawasan tersebut untuk membantu tim membuat keputusan yang tepat.
+//? Komunikasi dan eskalasi: Manajer proyek harus menjadi komunikator yang efektif, baik secara tertulis maupun lisan. Mereka harus dapat mengomunikasikan rencana proyek, memberikan pembaruan status, dan meningkatkan risiko atau masalah kepada pemangku kepentingan
+//? Fleksibilitas: Manajer proyek harus dapat beradaptasi dengan perubahan. Rencana pasti akan berubah, bahkan dengan perencanaan awal yang matang. Manajer proyek yang baik tahu bagaimana menghadapi perubahan yang tidak terduga ini dan menyesuaikan rencana proyek sesuai kebutuhan.
+//? Keterampilan organisasi yang kuat: Manajer proyek harus dapat mengatur banyak proses yang berbeda yang terlibat dalam suatu proyek. Ini termasuk melacak tugas, mengelola sumber daya, dan memastikan bahwa semua orang mengetahui peran dan tanggung jawab mereka.
+/// Kompetensi kunci: Fleksibilitas dan penanganan ambiguitas
+//? Memungkinkan pengambilan keputusan; mendorong anggota tim merasa lebih berdaya sejak proyek dimulai, seperti ambil keputusan bersama
+//? Berkomunikasi dan meningkatkan eskalasi; berkomunikasi secara efektif dan kapan harus mengangkat permasalahan ke manajemen
+//? Keterampilan organisasi yang kuat; seperti memanfaatkan banyak tool organisasi (templat, apps kolaborasi, evaluasi, laporan etc)
+//? Fleksibilitas; mengatasi berbagai perubahan dan tantangan
+//# Strategi perencanaan yang fleksibel; 
+//? Menilai kendala eksternal (seperti hari libur nasional dan anggota tim yang mengambil cuti liburan serta sakit)
+//? Merencanakan risiko dan tantangan; mempertimbangkan risiko yang mungkin terjadi, daftar orang-orang yang mungkin dapat bergabung dengan tim Anda jika salah satu anggota tim Anda tidak ada
+//? Menghitung "float" (kelonggaran) dalam jadwal Anda
+/// Mitos umum tentang manajer proyek
+//? Peran Anda sebagai manajer proyek adalah untuk berkomunikasi dengan pemangku kepentingan Anda, memperjelas tujuan, dan menetapkan ekspektasi.
+//? Percayakan tim Anda untuk menangani detail setiap tugas proyek dan berkomunikasi dengan Anda saat menemui masalah. 
+//? Melalui komunikasi langsung dan pendekatan strategis untuk pemecahan masalah, Anda dapat memberikan solusi dan membantu menghilangkan hambatan bagi tim Anda. 
+/// Kepemimpinan dan dinamika tim:
+//? Kepemimpinan: Kepemimpinan adalah proses memengaruhi orang lain untuk mencapai tujuan bersama. Pemimpin yang efektif memiliki visi yang jelas, mampu memotivasi orang lain, dan mampu membuat keputusan yang baik.
+//? Dinamika tim: Dinamika tim adalah cara anggota tim berinteraksi satu sama lain. Tim yang efektif memiliki komunikasi yang terbuka, saling percaya, dan saling mendukung.
+//? Keterampilan interpersonal: Keterampilan interpersonal adalah kemampuan untuk membangun hubungan dengan orang lain. Keterampilan interpersonal penting untuk kepemimpinan dan dinamika tim.
+//? Mempengaruhi tanpa otoritas: Mempengaruhi tanpa otoritas adalah kemampuan untuk membimbing orang lain untuk menyelesaikan pekerjaan mereka tanpa bertindak sebagai manajer langsung mereka. Ini membutuhkan keterampilan interpersonal yang kuat, seperti komunikasi, negosiasi, mediasi konflik, dan pemahaman motivasi.
